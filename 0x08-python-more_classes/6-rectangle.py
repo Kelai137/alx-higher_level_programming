7226d92a71f2fd4ac1e24d06e5b66996834490fa
Kelai137 <moseskelaisakala137@gmail.com>
2023-04-05 10:07:07 -0700

    updated task work for 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-# 6-rectangle.py
-#Kelai137 of ALX
 
 """defines a rectangle class"""
 
 class Rectangle:
+     """Represent a rectangle.
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+    """
+
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
     def __str__(self):
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
+        """
         if self.width == 0 or self.height == 0:
             return ""
         rect = ""
@@ -28,10 +40,12 @@ class Rectangle:
         return rect
 
     def __repr__(self):
+        """Return the string representation of the Rectangle."""
         return "Rectangle({}, {})".format(self.width, self.height)
 
     @property
     def width(self):
+        """set the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -45,6 +59,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """set the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -57,9 +72,11 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """Return the area of the Rectangle."""
         return self.width * self.height
 
     def perimeter(self):
+        """Return the perimeter of the Rectangle."""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
